41c1b2329daab9bed7dd62c58c2d05f2b505661a
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-10-17 21:03:18 +0800

    fetch data from laravel server

--- laravel-api-demo/routes/api.php
+++ laravel-api-demo/routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', function () {
+    return response()->json([
+        ['id' => 1, 'title' => 'First', 'message' => 'First Test API'],
+        ['id' => 2, 'title' => 'Second', 'message' => 'I am so dumb!'],
+    ]);
+
+});
