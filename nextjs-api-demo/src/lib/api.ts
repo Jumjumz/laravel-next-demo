aac7091a7a753e8ed0d4effa22d961f5a42aacdb
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-10-18 22:21:27 +0800

    https config

--- nextjs-api-demo/src/lib/api.ts
+++ nextjs-api-demo/src/lib/api.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 import https from "https";
 
-/*const agent = new https.Agent({
-    rejectUnauthorized: true,
-});*/
+const agent = new https.Agent({
+    rejectUnauthorized: false, // ignore cert ! WARNING: DO NOT INCLUDE IN PROD ! 
+});
 
 export default axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    //httpAgent: agent,
+    httpsAgent: agent,
     headers: {
-        "X-Requested-With" : "XMLHttpRequest"
+        "X-Requested-With" : "XMLHttpRequest",
+        "Content-Type": "application/json",
     },
     withCredentials: true
 });
