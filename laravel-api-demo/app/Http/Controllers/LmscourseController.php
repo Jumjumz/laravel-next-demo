a323b7bc9d9eec0e373665eeedf911932f0dcc7d
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-10-19 22:04:32 +0800

    add controller config

--- laravel-api-demo/app/Http/Controllers/LmscourseController.php
+++ laravel-api-demo/app/Http/Controllers/LmscourseController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Lmscourse;
+
+//use Illuminate\Http\Request;
 
 class LmscourseController extends Controller
 {
-    //
+    public function index() {
+        $lmscourses = Lmscourse::all();
+        return response()->json($lmscourses);
+    }
 }
