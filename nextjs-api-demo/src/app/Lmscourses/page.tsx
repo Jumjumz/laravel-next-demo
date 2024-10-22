2175abc9879b54f4d1d211f968c8b89a29607377
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-10-22 20:19:20 +0800

    return table

--- nextjs-api-demo/src/app/Lmscourses/page.tsx
+++ nextjs-api-demo/src/app/Lmscourses/page.tsx
@@ -19,7 +19,7 @@ interface LmsCourses {
 
 async function getAxiosPost(): Promise<LmsCourses[] | undefined> {
   try {
-    const response = await api.get("/sanctum/csrf-cookie");
+    const response = await api.get("/lmscourse");
     console.log(response.headers);
     return response.data;
   } catch (err) {
@@ -32,7 +32,29 @@ export default async function LmsCourses() {
   let lmscourse = await getAxiosPost();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Table className=" text-xl text-white font-mono w-[100rem]">
+          <TableCaption>Course List</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Course Name</TableHead>
+              <TableHead>Course Type</TableHead>
+              <TableHead className=" text-right">Course Completion</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {lmscourse?.map((course) => (
+              <TableRow key={course._id}>
+                <TableCell>{course.courseName}</TableCell>
+                <TableCell>{course.courseType}</TableCell>
+                <TableCell className=" text-right">
+                  {course.courseWeeksCompletion}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </main>
     </div>
   );
 }
