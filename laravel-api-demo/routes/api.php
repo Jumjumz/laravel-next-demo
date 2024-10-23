ca4549106f459e73d805a9774d2967c4cc12c4d6
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-10-23 16:05:15 +0800

    test

--- laravel-api-demo/routes/api.php
+++ laravel-api-demo/routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\LmscourseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Lmscourse;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
