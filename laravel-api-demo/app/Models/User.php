73f828038410e342cc7a8a6d37e3a41f72a3e043
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-10-25 03:55:27 +0800

    tokens init

--- laravel-api-demo/app/Models/User.php
+++ laravel-api-demo/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 //use Illuminate\Foundation\Auth\User as Authenticatable;
 use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use MongoDB\Laravel\Eloquent\Model;
 
 class User extends Authenticatable
