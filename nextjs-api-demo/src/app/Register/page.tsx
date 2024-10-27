cc507be74ddf70df99d969aa705e99d855cb4dc7
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-10-27 19:40:17 +0800

    create register button and function

--- nextjs-api-demo/src/app/Register/page.tsx
+++ nextjs-api-demo/src/app/Register/page.tsx
@@ -9,31 +9,39 @@ interface Register {
   password: string;
 }
 
-async function postAxiosRegister() {
-  const [message, setMessage] = useState<string | null>();
-  //const [resData, setResData] = useState(Promise<Register | undefined>);
-  try {
-    const response = await api.post(
-      "/register",
-      {
-        email: "jumzfromnextButton@gmail.com",
-        password: "1234",
-      },
-      { withCredentials: true }
-    );
-    setMessage("New User has been Registered");
-    //setResData(response.data);
-  } catch (err) {
-    console.error("Post failed", err);
-    setMessage("Failed Registering the user");
-  }
-}
-
 export default function Register() {
   //const [register, setRegister] = useState({});
+  const [message, setMessage] = useState<string | null>();
+  async function postAxiosRegister() {
+    //const [resData, setResData] = useState(Promise<Register | undefined>);
+    try {
+      await api.post(
+        "/register",
+        {
+          email: "jumzfromnextButton@gmail.com",
+          password: "1234",
+        },
+        { withCredentials: true }
+      );
+      setMessage("New User has been Registered");
+    } catch (err) {
+      console.error("Post failed", err);
+      setMessage("Failed to Register");
+    }
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h2 className="text-center text-white">Register a user</h2>
+        <button
+          onClick={postAxiosRegister}
+          className="w-44 h-16 bg-white rounded-md"
+        >
+          Register a User
+        </button>
+        <h4 className="text-center text-white">{message}</h4>
+      </main>
     </div>
   );
 }
