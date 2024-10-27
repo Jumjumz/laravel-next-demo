d2597dc52732dd36db2a337468720ba3b3fc849c
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-10-27 21:34:10 +0800

    form creating

--- nextjs-api-demo/src/app/Register/page.tsx
+++ nextjs-api-demo/src/app/Register/page.tsx
@@ -35,23 +35,29 @@ export default function Register({ email, password }: Register) {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h2 className="w-full text-center text-white">Register a User</h2>
-        <form method="POST">
-          <input type="email" value={email} placeholder="Email" required />
-          <input
-            type="password"
-            value={password}
-            placeholder="Password"
-            required
-          />
-          <button
-            onClick={postAxiosRegister}
-            className="w-44 h-16 bg-white rounded-md"
-          >
-            Register a User
-          </button>
-        </form>
-        <h4 className="w-full text-center text-white">{message}</h4>
+        <div className=" w-80 h-72">
+          <form className="flex flex-row flex-wrap w-full gap-4 justify-center">
+            <h2 className="w-full text-center text-white">Register a User</h2>
+            <input
+              type="email"
+              value={email}
+              placeholder="Email"
+              required
+              className="w-full h-8"
+            />
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              required
+              className="w-full h-8"
+            />
+            <button type="submit" className="w-full h-8 bg-white rounded-md">
+              Register
+            </button>
+          </form>
+          <h4 className="w-full text-center text-white">{message}</h4>
+        </div>
       </main>
     </div>
   );
