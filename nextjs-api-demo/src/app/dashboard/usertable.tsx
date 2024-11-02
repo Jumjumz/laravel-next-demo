23db08d33eec256ffd7d3b0a5b3a35e44a859684
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-02 19:41:55 +0800

    style user table component

--- nextjs-api-demo/src/app/dashboard/usertable.tsx
+++ nextjs-api-demo/src/app/dashboard/usertable.tsx
@@ -39,26 +39,22 @@ export default function UserTable() {
   }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Table className=" text-xl text-white font-mono w-[100rem]">
-          <TableCaption>Dashboard User List</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Email</TableHead>
-              <TableHead>Password</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {users?.map((user) => (
-              <TableRow key={user._id}>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>{user.password}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </main>
-    </div>
+    <Table className=" text-xl text-white font-mono w-[100rem]">
+      <TableCaption>Dashboard User List</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Email</TableHead>
+          <TableHead>Password</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users?.map((user) => (
+          <TableRow key={user._id}>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.password}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
