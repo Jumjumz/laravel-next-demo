1a0fc9866fba0a0f4aa6d191637969c9cc1b5563
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-02 19:36:50 +0800

    update dashboard

--- nextjs-api-demo/src/app/dashboard/page.tsx
+++ nextjs-api-demo/src/app/dashboard/page.tsx
@@ -1,65 +1,7 @@
-"use client";
-
-import api from "@/lib/api";
-import Logout from "./logout";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { useEffect, useState } from "react";
-
-interface User {
-  _id: string;
-  email: string;
-  password: string;
-}
-
-async function getAxiosUser(): Promise<User[] | undefined> {
-  try {
-    const response = await api.get("/users", { withCredentials: true });
-    //console.log(response.headers);
-    return response.data;
-  } catch (err) {
-    console.error("Fetch failed", err);
-    return undefined;
-  }
-}
-
 export default function Dashboard() {
-  const [users, setUsers] = useState<User[] | undefined>();
-
-  useEffect(() => {
-    getAxiosUser().then((response) => {
-      setUsers(response);
-    });
-  }, []);
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Table className=" text-xl text-white font-mono w-[100rem]">
-          <TableCaption>Dashboard User List</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Email</TableHead>
-              <TableHead>Password</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {users?.map((user) => (
-              <TableRow key={user._id}>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>{user.password}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
     </div>
   );
 }
