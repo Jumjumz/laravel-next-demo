108d560d8b63158e031f18beba54f37807166c2d
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-02 19:23:00 +0800

    add user table component

---
+++ nextjs-api-demo/src/app/dashboard/usertable.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import api from "@/lib/api";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useEffect, useState } from "react";
+
+export default function UserTable() {}
