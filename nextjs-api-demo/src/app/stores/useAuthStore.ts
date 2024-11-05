e036fa573db489848c495988a6aaa41c8e76f8c0
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-05 21:10:30 +0800

    change zustand config to persist states

--- nextjs-api-demo/src/app/stores/useAuthStore.ts
+++ nextjs-api-demo/src/app/stores/useAuthStore.ts
@@ -1,12 +1,28 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 
 interface AuthStore {
     email : string,
     setEmail : (email : string) => void;
+    clearEmail : () => void;
 }
 
-export const useAuthStore = create<AuthStore> ((set) => ({
+/*export const useAuthStore = create<AuthStore> ((set) => ({
     email: "",
     setEmail : (email : string) => set(() => ({ email })), 
-}));
+}));*/
+
+export const useAuthStore = create<AuthStore>()(
+    persist(
+        (set) => ({
+            email: "",
+            setEmail : (email : string) => set(() => ({ email })),
+            clearEmail : () => set(() => ({ email : " "})),
+        }),
+        {
+            name: 'auth-storage',
+            storage: createJSONStorage(() => localStorage), // syntax for storing to localstorage
+        }
+    )
+)
