a073ebe9d79eb084b03d6dfca4d21585f87be9e1
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-05 19:46:49 +0800

    zustand config

--- nextjs-api-demo/src/app/stores/useAuthStore.ts
+++ nextjs-api-demo/src/app/stores/useAuthStore.ts
@@ -1,3 +1,12 @@
-import create from "zustand";
+import { create } from "zustand";
 
 
+interface AuthStore {
+    email : string,
+    setEmail : (email : string) => void;
+}
+
+export const useAuthStore = create<AuthStore> ((set) => ({
+    email: "",
+    setEmail : (email : string) => set(() => ({ email })), 
+}));
