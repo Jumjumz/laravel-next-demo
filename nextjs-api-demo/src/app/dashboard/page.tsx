aa4fccb884700f7b6cece4a618dd8e775857a206
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-06 19:33:07 +0800

    style nav bar

--- nextjs-api-demo/src/app/dashboard/page.tsx
+++ nextjs-api-demo/src/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className=" w-full">
+        <div className=" w-full flex flex-row justify-between">
           <Header />
           <Logout />
         </div>
