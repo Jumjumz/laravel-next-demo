58ee631e8831dde6ba97163c8b2ee978f6179c8e
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-06 18:52:13 +0800

    add header to dashboard

--- nextjs-api-demo/src/app/dashboard/page.tsx
+++ nextjs-api-demo/src/app/dashboard/page.tsx
@@ -1,18 +1,14 @@
-"use client";
-
 import UserTable from "./usertable";
 import Logout from "./logout";
-import { useAuthPersist } from "../stores/useAuthStore";
+import Header from "../components/header";
 
 export default function Dashboard() {
-  const loginEmail = useAuthPersist((state) => state.email);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className=" w-full">
-          <h2 className=" text-white">Email: {loginEmail} </h2>
+          <Header />
         </div>
-        <Logout email={loginEmail} />
         <div className=" w-[100rem]">
           <UserTable />
         </div>
