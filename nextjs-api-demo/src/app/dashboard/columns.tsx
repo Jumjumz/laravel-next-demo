0ceb00f060f944b8851db7116f6bd5a72b3a5e30
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-10 21:32:01 +0800

    remove checkbox

--- nextjs-api-demo/src/app/dashboard/columns.tsx
+++ nextjs-api-demo/src/app/dashboard/columns.tsx
@@ -1,4 +1,4 @@
-import { ColumnDef, flattenBy } from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 import Delete from "./delete";
 import { Checkbox } from "@radix-ui/react-checkbox";
 
@@ -8,30 +8,6 @@ interface Users {
 }
 
 export const columns: ColumnDef<Users>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
-        aria-label="Select all"
-        className=" w-4 h-4 "
-      />
-    ),
-    cell: ({ row }) => {
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className=" wh-4 h-4 bg-white"
-      />;
-    },
-    enableSorting: false,
-    enableHiding: false,
-  },
   {
     accessorKey: "email",
     header: "Email",
