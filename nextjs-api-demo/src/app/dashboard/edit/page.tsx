4af9653177bda60d5f6a0c33186cd5504c8cc88f
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-12 21:01:44 +0800

    working put method

--- nextjs-api-demo/src/app/dashboard/edit/page.tsx
+++ nextjs-api-demo/src/app/dashboard/edit/page.tsx
@@ -8,28 +8,33 @@ import Email from "@/app/components/email";
 import { Metadata } from "next";
 import Link from "next/link";
 import { useAuthPut } from "@/app/stores/useAuthStore";
+import { useRouter } from "next/navigation";
 
 interface Users {
   id: string;
   email: string;
 }
 
-async function editAxiosUser({ id, email }: Users) {
-  try {
-    const response = await api.put(
-      `update/${id}`,
-      { email },
-      { withCredentials: true }
-    );
-    return response.data;
-  } catch (err) {
-    console.error("Failed to update");
-  }
-}
-
 export default function Edit() {
   const userId = useAuthPut((set) => set.id);
   const userEmail = useAuthPut((set) => set.email);
+
+  const router = useRouter();
+
+  async function editAxiosUser() {
+    try {
+      const response = await api.put(
+        `update/${userId}`,
+        { email: "jumzfromEdit@gmail.com" },
+        { withCredentials: true }
+      );
+      router.push("/dashboard");
+      return response.data;
+    } catch (err) {
+      console.error("Failed to update");
+    }
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center w-full sm:items-start">
@@ -46,6 +51,9 @@ export default function Edit() {
           >
             Back
           </Link>
+          <button onClick={editAxiosUser} className=" w-24 bg-blue-700">
+            Edit
+          </button>
         </div>
       </main>
     </div>
