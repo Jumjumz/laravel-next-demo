094672a8ce0a2aacf14d00b83e284842c394f608
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-12 20:52:01 +0800

    access email to edit page

--- nextjs-api-demo/src/app/dashboard/edit/page.tsx
+++ nextjs-api-demo/src/app/dashboard/edit/page.tsx
@@ -29,6 +29,7 @@ async function editAxiosUser({ id, email }: Users) {
 
 export default function Edit() {
   const userId = useAuthPut((set) => set.id);
+  const userEmail = useAuthPut((set) => set.email);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center w-full sm:items-start">
@@ -38,6 +39,7 @@ export default function Edit() {
         </nav>
         <div className=" w-full">
           <h2 className="text-white">{userId}</h2>
+          <h2 className=" text-white">{userEmail}</h2>
           <Link
             href="/dashboard"
             className=" w-24 h-8 bg-white text-black text-center"
