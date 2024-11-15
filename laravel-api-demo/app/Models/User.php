0145a094d8cc9208f0ff115bf862f63b9d0eb082
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-15 19:32:36 +0800

    change to name

--- laravel-api-demo/app/Models/User.php
+++ laravel-api-demo/app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
     protected $connection = 'mongodb';
     protected $collection = 'users';
     protected $fillable = [
-        'fullname',
+        'name',
         'username',
         'email',
         'password',
