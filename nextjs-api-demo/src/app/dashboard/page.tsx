ed56fbecf723f9a203a638646102bae4e78e0ca2
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-16 19:52:38 +0800

    add navbar to dashboard

--- nextjs-api-demo/src/app/dashboard/page.tsx
+++ nextjs-api-demo/src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import Logout from "./logout";
 import Email from "../components/email";
 import { Metadata } from "next";
 import { DataTable } from "./data-table";
+import Navbar from "../components/navbar";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -12,10 +13,7 @@ export default function Dashboard() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center w-full sm:items-start">
-        <nav className=" w-full flex flex-row justify-between">
-          <Email />
-          <Logout />
-        </nav>
+        <Navbar />
         <div className=" w-full">
           <DataTable />
         </div>
