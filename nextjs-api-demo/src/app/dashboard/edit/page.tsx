453d67ff750b9036aefd4a52b27d0d7b18ce7551
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-16 19:54:03 +0800

    navbar in edit page

--- nextjs-api-demo/src/app/dashboard/edit/page.tsx
+++ nextjs-api-demo/src/app/dashboard/edit/page.tsx
@@ -2,13 +2,11 @@
 
 import api from "@/lib/api";
 
-import Logout from "../logout";
-import Email from "@/app/components/email";
-
 import Link from "next/link";
 import { useAuthUpdate } from "@/app/stores/useAuthStore";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Navbar from "@/app/components/navbar";
 
 interface Users {
   id: string;
@@ -48,10 +46,7 @@ export default function Edit() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-editName:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center w-full sm:items-start">
-        <nav className=" w-full flex flex-row justify-between">
-          <Email />
-          <Logout />
-        </nav>
+        <Navbar />
         <div className=" flex flex-col w-full gap-4">
           <form
             method="PUT"
