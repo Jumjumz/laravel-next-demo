1d9358e8a27ff7b5da20bbda58291c7a432ed32a
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-19 19:57:21 +0800

    spacing between form and button

--- nextjs-api-demo/src/app/login/page.tsx
+++ nextjs-api-demo/src/app/login/page.tsx
@@ -47,7 +47,7 @@ export default function Login() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className=" w-80 h-72">
+        <div className=" w-80 h-72 flex flex-col gap-4">
           <form
             onSubmit={postAxiosLogin}
             method="POST"
