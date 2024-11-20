61c72e46d888e1d45cba80adf412cd5f6d09efed
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-20 20:59:02 +0800

    add clear role in logout button

--- nextjs-api-demo/src/app/dashboard/logout.tsx
+++ nextjs-api-demo/src/app/dashboard/logout.tsx
@@ -11,12 +11,15 @@ interface User {
 export default function Logout() {
   const email = useAuthPersist((state) => state.email);
   const clearEmail = useAuthPersist((state) => state.clearEmail);
+  const clearRole = useAuthPersist((state) => state.clearRole);
+
   const router = useRouter();
 
   const logout = async () => {
     try {
       await api.post("/logout", { email: email }, { withCredentials: true });
       clearEmail();
+      clearRole();
       router.push("/login");
     } catch (err) {
       console.error("Fetch failed", err);
