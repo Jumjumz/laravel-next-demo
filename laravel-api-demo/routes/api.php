3f7d0d36565f967f28e051a9a04efbff67d872d1
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-20 18:50:26 +0800

    remove test get

--- laravel-api-demo/routes/api.php
+++ laravel-api-demo/routes/api.php
@@ -9,16 +9,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts', function () {
-    return response()->json([
-        ['id' => 1, 'title' => 'First', 'message' => 'First Test API'],
-        ['id' => 2, 'title' => 'Second', 'message' => 'I am so dumb!'],
-        ['id' => 3, 'title' => 'WORKING!', 'message' => 'I am PRO!'],
-        ['id' => 4, 'title' => 'Samples', 'message' => 'Finally!'],
-    ]);
-
-});
-
 Route::get('/lmscourse', [LmscourseController::class, 'index']); // get courses
 
 Route::post('/lmscourse/create', [LmscourseController::class, 'create' ]); // create courses
