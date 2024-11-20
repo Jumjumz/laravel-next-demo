a8f6ec54270e78b3629e984551d5fd80afc791aa
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-20 18:52:03 +0800

    comment out defauly setting

--- laravel-api-demo/routes/api.php
+++ laravel-api-demo/routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+/*Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 Route::get('/sanctum/csrf-cookie', function () {
     return response()->json('CSRF TOKEN');
