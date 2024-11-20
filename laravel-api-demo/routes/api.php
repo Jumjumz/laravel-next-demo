8c8347ca87eae29ae8ae60ba484234dbb81a97c5
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-20 18:51:14 +0800

    remove lmscontroller

--- laravel-api-demo/routes/api.php
+++ laravel-api-demo/routes/api.php
@@ -9,9 +9,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/lmscourse', [LmscourseController::class, 'index']); // get courses
-
-Route::post('/lmscourse/create', [LmscourseController::class, 'create' ]); // create courses
 Route::get('/sanctum/csrf-cookie', function () {
     return response()->json('CSRF TOKEN');
 });
