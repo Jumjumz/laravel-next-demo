6b225f3a56f167ed4825a1cd43f04d5ba5ecb656
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-22 20:49:23 +0800

    update app.php for websockets

--- laravel-api-demo/bootstrap/app.php
+++ laravel-api-demo/bootstrap/app.php
@@ -27,4 +27,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withBroadcasting(
+        __DIR__.'/../routes/channels.php',
+        ['prefix', 'api', 'middleware' => ['api', 'auth:sanctum']],
+    )
+    ->create();
