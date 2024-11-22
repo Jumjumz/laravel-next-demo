b77ad2b27e7a125a74900d3e6ec7232212ebed2a
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-22 20:24:48 +0800

    echo config

--- nextjs-api-demo/src/lib/echo.ts
+++ nextjs-api-demo/src/lib/echo.ts
@@ -1,3 +1,15 @@
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 
+
+const echo = new Echo({
+    broadcaster: "pusher",
+    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+    wsHost: process.env.NEXT_PUBLIC_WS_HOST,
+    wsPort: 8080,
+    forceTLS: false,
+    disableStats: true,
+
+});
+
+export default echo;
