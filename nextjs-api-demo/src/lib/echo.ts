2b2427304a6bcb7a74087dcd83b5c68bb40a9927
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-22 20:32:13 +0800

    update echo config

--- nextjs-api-demo/src/lib/echo.ts
+++ nextjs-api-demo/src/lib/echo.ts
@@ -6,7 +6,7 @@ const echo = new Echo({
     broadcaster: "pusher",
     key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
     wsHost: process.env.NEXT_PUBLIC_WS_HOST,
-    wsPort: process.env.NEXT_PUBLIC_HOST_PORT,
+    wsPort: process.env.NEXT_PUBLIC_HOST_PORT ?? 80,
     forceTLS: false,
     disableStats: true,
 
