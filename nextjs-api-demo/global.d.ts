0a796b6fc4427ed6d382f583fcfe11d1ec1e7909
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-24 21:39:39 +0800

    config for ts to accept pusher

---
+++ nextjs-api-demo/global.d.ts
@@ -0,0 +1,9 @@
+export {};
+
+import Pusher from "pusher-js";
+
+declare global {
+  interface Window {
+    Pusher: typeof Pusher;
+  }
+}
--- nextjs-api-demo/src/lib/echo.ts
+++ nextjs-api-demo/src/lib/echo.ts
@@ -1,37 +1,41 @@
-import axios from "axios";
-
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
+
+import api from "./api";
 
 window.Pusher = Pusher;
 
 const echo = new Echo({
-    broadcaster: "reverb",
-    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
-    cluster: "mt1",
-    wsHost: process.env.NEXT_PUBLIC_WS_HOST,
-    wsPort: process.env.NEXT_PUBLIC_HOST_PORT ?? 80,
-    encrypted: true,
-    forceTLS: false,
-    disableStats: true,
-    enabledTransports: ['ws', 'was'],
-    authorizer: ({channel, options} : any) => {
-        return {
-            authorize: ({socketId, callback} : any) => {
-                axios.post('api/broadcasting/auth', {
-                    socket_Id : socketId,
-                    channel_name : channel.name
-                })
-                .then(response => {
-                    callback(false, response.data);
-                })
-                .catch(error => {
-                    callback(true, error);
-                })
-            }
-        }
-    }
-
+  broadcaster: "reverb",
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+  wsHost: process.env.NEXT_PUBLIC_WS_HOST,
+  wssHost: process.env.NEXT_PUBLIC_WS_HOST,
+  wsPort: process.env.NEXT_PUBLIC_HOST_PORT ?? 80,
+  wssPort: process.env.NEXT_PUBLIC_HOST_PORT ?? 443,
+  forceTLS: (process.env.NEXT_PUBLIC_SCHEME ?? "https") === "https",
+  encrypted: false,
+  disableStats: true,
+  enabledTransports: ["ws", "wss"],
+  options: {
+    rejectUnauthorized: false, // ignore ssl cert
+  },
+  authorizer: ({ channel, options }: any) => {
+    return {
+      authorize: ({ socketId, callback }: any) => {
+        api
+          .post("/api/broadcasting/auth", {
+            socket_Id: socketId,
+            channel_name: channel.name,
+          })
+          .then((response) => {
+            callback(false, response.data);
+          })
+          .catch((error) => {
+            callback(true, error);
+          });
+      },
+    };
+  },
 });
 
 export default echo;
