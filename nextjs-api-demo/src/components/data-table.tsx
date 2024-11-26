2e44d026f8cd67d92ccb03e632502059337a8a38
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-26 18:42:29 +0800

    make column an array

--- nextjs-api-demo/src/components/data-table.tsx
+++ nextjs-api-demo/src/components/data-table.tsx
@@ -19,7 +19,7 @@ import {
 } from "@/components/ui/table";
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>;
+  columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }
 
