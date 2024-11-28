01f53ef7b3b51232e3454676e18023ae96967759
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-11-28 23:21:11 +0800

    remove unused import

--- nextjs-api-demo/src/app/components/email.tsx
+++ nextjs-api-demo/src/app/components/email.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useAuthPersist, useAuthUpdate } from "../stores/useAuthStore";
+import { useAuthPersist } from "../stores/useAuthStore";
 
 export default function Email() {
   const email = useAuthPersist((state) => state.email);
