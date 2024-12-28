c00519566357b2ff5954bad9e3c6c11abac24ea6
Jumjumz <jumjumramos@gmail.com>
2024-12-28 23:42:41 +0800

    change button in home page

--- nextjs-api-demo/src/app/page.tsx
+++ nextjs-api-demo/src/app/page.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
           <Link href="/login">
-            <button className=" w-24 h-8 bg-white text-black">Login</button>
+            <Button className=" w-24 h-8" variant="secondary">
+              Login
+            </Button>
           </Link>
         </div>
         <div>
           <Link href="/register">
-            <button className=" w-24 h-8 bg-white text-black">Register</button>
+            <Button className=" w-24 h-8">Register</Button>
           </Link>
         </div>
       </main>
